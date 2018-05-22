35ca1637a6f3760fd495a4f61f4675040cb838b3
Nicolai Moraru <dot.nick.dot.messing@gmail.com>
2018-05-22 09:57:24 +0300

    Update halo.js and add mainnet

--- src/constants.js
+++ src/constants.js
@@ -8,7 +8,8 @@ export const NETWORKS = {
   '5777': 'Eth - Ganache Blockchain',
   '425672638': 'Halo - QA Network',
   '4256338638': 'Halo - Developer Network',
-  '42568378638': 'Halo - Test Network'
+  '42566246638': 'Halo - Main Network',
+  '42568378638': 'Halo - Test Network',
 }
 
 export const APPROVED_NETWORK_ID = '5777'
